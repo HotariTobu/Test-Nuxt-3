2eda2d03f2ecfa3ea36e09bcc47d8a21a4b22031
HotariTobu <hotari24tools@gmail.com>
2023-11-29 21:19:34 +0900

    add: page reload

---
+++ pages/reload.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const router = useRouter()
+const count = ref(0)
+
+const reload = () => {
+  router.go(0)
+}
+</script>
+
+<template>
+  <div>
+    <button @click="count++">{{ count }}</button>
+    <button @click="reload">Reload</button>
+  </div>
+</template>
